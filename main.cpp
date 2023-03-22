37aa30adab7e9eb2f468b7b66cc718befcc1e58d
DieR-G <00043920@uca.edu.sv>
2023-03-22 14:05:06 -0600

    add cli arguments

--- main.cpp
+++ main.cpp
@@ -6,14 +6,16 @@
 
 using namespace std;
 
-directed_graph create_graph(){
+directed_graph create_graph()
+{
     ifstream file;
     file.open("test_graph.csv");
     string current_line;
     getline(file, current_line);
     unsigned int nodes = stoul(current_line);
     directed_graph graph(nodes);
-    while(getline(file, current_line)){
+    while (getline(file, current_line))
+    {
         stringstream line_stream(current_line);
         string from, to, cost;
         getline(line_stream, from, ',');
@@ -25,11 +27,17 @@ directed_graph create_graph(){
     return graph;
 }
 
-int main(){
-    directed_graph my_graph = create_graph();
-    auto result = my_graph.dijkstra(0,155);
-    for(auto x : result){
-        cout << x << ' ';
+int main(int argc, char *argv[])
+{
+    if (argc == 3)
+    {
+        directed_graph my_graph = create_graph();
+        //76 272
+        auto result = my_graph.dijkstra(atoi(argv[1]), atoi(argv[2]));
+        for (auto x : result)
+        {
+            cout << x << ' ';
+        }
     }
     return 0;
 }
